a5ea0a0dd2df78837784c5558928e277b6863073
danielk <kucharski22daniel@gmail.com>
2018-04-22 19:56:59 +0200

    Added reading of file with stdlib fopen, reading line with fgetln

--- test_iphone7/main.m
+++ test_iphone7/main.m
@@ -12,14 +12,34 @@
 int main(int argc, char * argv[]) {
         
     @autoreleasepool {
+        int ret;
         NSLog(@" starting ... ");
         
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5*NSEC_PER_SEC), dispatch_get_main_queue(), ^{
             NSLog(@"\n\n MAIN : after 5 \n\n");
+            printf(" std lib ??? \n");
+            FILE *file;
+            
+            NSString *filePath = [[NSBundle mainBundle] pathForResource:@"README" ofType:@"md"];
+            printf(" file Path %s \n ",[filePath UTF8String]);
+            
+            printf("path: %s ",[filePath UTF8String]);
+            
+            file = fopen([filePath UTF8String], "r");
+            if (NULL != file)
+            {
+                char *tab;
+                size_t  len;
+                tab = fgetln(file, &len);
+                
+                printf("from file: \n %s \n",tab);
+                printf(" I read %ld bytes \n",len);
+                fclose(file);
+            }
             
         });
-        
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        ret = UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return ret;
     }
     
 }
